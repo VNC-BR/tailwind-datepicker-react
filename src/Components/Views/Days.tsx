70c94feb1097cd6fad5478ca5694e3ddb4d45693
Lucas Ortigoso <lucas.ortigoso@gmail.com>
2024-08-25 10:41:24 -0300

    fix themes

--- src/Components/Views/Days.tsx
+++ src/Components/Views/Days.tsx
@@ -9,7 +9,7 @@ interface IDaysProps {
 
 const Days = ({ start }: IDaysProps) => {
 	const { selectedDate, changeSelectedDate, showSelectedDate, getFormattedDate, options } = useContext(DatePickerContext)
-	
+
 	const startOfWeek = (new Date(start).getDay() + 6) % 7;
 	const weekDays = options.weekDays || [];
 	const sortedWeekDays = weekDays.slice(startOfWeek).concat(weekDays.slice(0, startOfWeek));
@@ -29,21 +29,33 @@ const Days = ({ start }: IDaysProps) => {
 					const day = getFormattedDate(current, { day: "numeric" })
 					const month = getFormattedDate(current, { month: "long" })
 					const year = getFormattedDate(current, { year: "numeric" })
+
+					// check if date is disabled
+					const isDisabled =
+						(options?.minDate && new Date(current) < options?.minDate) ||
+						(options?.maxDate && new Date(current) > options?.maxDate) ||
+						(options?.disabledDates && options.disabledDates.indexOf(new Date(current)) >= 0)
+
+					// check if date is selected
+					const isSelectedDate = showSelectedDate && selectedDate.getTime() > 0 && getFormattedDate(selectedDate) === getFormattedDate(current)
+
+					// check if date is current month/year
+					const isCurrentMonthAndYear = month === getFormattedDate(selectedDate, { month: "long" }) && year === getFormattedDate(selectedDate, { year: "numeric" })
+
 					return (
 						<span
 							key={index}
-							className={`hover:bg-gray-100 dark:hover:bg-gray-600 block flex-1 leading-9 border-0 rounded-lg cursor-pointer text-center  dark:text-white font-semibold text-sm ${
-								showSelectedDate && selectedDate.getTime() > 0 && getFormattedDate(selectedDate) === getFormattedDate(current)
+							className={twMerge(
+								"hover:bg-gray-100 dark:hover:bg-gray-600 font-semibold text-sm cursor-pointer text-center leading-9 dark:text-white border-0 block flex-1 rounded-lg",
+								isDisabled
+									? twMerge("text-gray-500", options?.theme?.disabledText)
+									: isSelectedDate
 									? twMerge("bg-blue-700 text-white hover:bg-blue-600", options?.theme?.selected)
-									: ""
-							} ${
-								month == getFormattedDate(selectedDate, { month: "long" }) && year == getFormattedDate(selectedDate, { year: "numeric" })
+									: isCurrentMonthAndYear
 									? twMerge("text-gray-900", options?.theme?.text)
-									: twMerge("text-gray-500", options?.theme?.dateOtherMonth ? options.theme?.dateOtherMonth : options?.theme?.disabledText)
-							} ${(options?.minDate && new Date(current) < options?.minDate) || (options?.disabledDates && options.disabledDates.indexOf(new Date(current)) >= 0) ? twMerge("text-gray-500", options?.theme?.disabledText) : ""} ${
-								(options?.maxDate && new Date(current) > options?.maxDate) || (options?.disabledDates && options.disabledDates.indexOf(new Date(current)) >= 0) ? twMerge("text-gray-500", options?.theme?.disabledText) : ""
-							}
-                            `}
+									: twMerge("text-gray-500", options.theme?.dateOtherMonth),
+									""
+							)}
 							onClick={() => {
 								changeSelectedDate("date", new Date(current))
 							}}
